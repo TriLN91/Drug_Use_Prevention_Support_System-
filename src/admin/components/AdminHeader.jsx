5b407bd5e0ae6ca0f1f16a8599e70320c892178a
HoangNDxFPT <hoangnguyenduc674@gmail.com>
2025-06-04 08:27:55 +0700

    create

--- src/admin/components/AdminHeader.jsx
+++
@@ -1,8 +0,0 @@
-export default function AdminHeader() {
-  return (
-    <header className="admin-header">
-      <span>Drug Use Prevention - ADMIN PANEL</span>
-      <span>Hello, Admin User <button>Log out</button></span>
-    </header>
-  );
-}
