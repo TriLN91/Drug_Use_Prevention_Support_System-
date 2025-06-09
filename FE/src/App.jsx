5b290f7a4a0bfe39669f471d7a00c89d414d3044
2octddarwin <khoivtse184567@fpt.edu.vn>
2025-06-09 18:27:10 +0700

    code moi cho hoang

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -3,20 +3,9 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { store, persistor } from './redux/store';
-
-import HomePage from './page/HomePage';
-import LoginPage from './login/LoginPage';
-import RegisterPage from './login/RegisterPage';
-import CouresListPage from './page/CouresListPage';
-import UserProfilePage from './page/UserProfilePage';
-import Servey from './page/Servey';
-import CourseVideo from './page/CourseVideo';
-import CourseQuiz from './page/CourseQuiz';
-import AdminLayout from './admin/AdminLayout';
-import Dashboard from './admin/page/Dashboard';
-import UserManage from './admin/page/UserManage';
-import CourseManage from './admin/page/CourseManage';
-
+import HomePage from './member/page/HomePage';
+import LoginPage from './pages/login';
+import RegisterPage from './pages/register';
 function RequireAdmin({ children }) {
   const user = JSON.parse(localStorage.getItem("user") || "{}");
   if (!user || user.role_id !== 1) return <LoginPage />;
@@ -36,40 +25,7 @@ const router = createBrowserRouter([
     path: "/register",
     element: <RegisterPage />,
   },
-  {
-    path: "/courseList",
-    element: <CouresListPage />,
-  },
-  {
-    path: "/userProfile",
-    element: <UserProfilePage />,
-  },
-  {
-    path: "/course",
-    element: <CourseVideo />,
-  },
-  {
-    path: "/servey",
-    element: <Servey />,
-  },
-  {
-    path: "/quiz",
-    element: <CourseQuiz />,
-  },
-  {
-    path: "/admin",
-    element: (
-      <RequireAdmin>
-        <AdminLayout />
-      </RequireAdmin>
-    ),
-    children: [
-      { index: true, element: <Dashboard /> },
-      { path: "users", element: <UserManage /> },
-      { path: "courses", element: <CourseManage /> },
-      { path: "profile", element: <UserProfilePage /> },
-    ],
-  },
+
 ]);
 
 function App() {
